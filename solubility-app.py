185a8d6f4cc0388345a426e2c4902f23588e0f1c
punitjain-jp <76389204+punitjain-jp@users.noreply.github.com>
2022-03-05 04:09:44 +0530

    Update solubility-app.py

--- solubility-app.py
+++ solubility-app.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import pickle
+import pickle5 as pickle
 from PIL import Image
 from rdkit import Chem
 from rdkit.Chem import Descriptors
